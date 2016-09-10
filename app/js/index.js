94fb2e2efce1379bbab009030882921e0d666d40
Adam Gonzales <adamgonzales1@gmail.com>
2016-09-10 09:44:16 -0700

    update todo methods, change todos to objects

--- app/js/index.js
+++ app/js/index.js
@@ -1,18 +1,26 @@
 var todoList = {
-	todos: ['item 1', 'item 2', 'item 3'],
+	todos: [],
 	displayTodos: function() {
 		console.log('My Todos: ', this.todos);
 	},
-	addTodo: function(todo) {
-		this.todos.push(todo);
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
 		this.displayTodos();
 	},
-	changeTodo: function(pos, newValue) {
-		this.todos[pos] = newValue;
+	changeTodo: function(pos, todoText) {
+		this.todos[pos].todoText = todoText;
 		this.displayTodos();
 	},
 	deleteTodo: function(pos) {
 		this.todos.splice(pos, 1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(pos) {
+		var todo = this.todos[pos];
+		todo.completed = !todo.completed;
 		this.displayTodos();
 	}
 };
